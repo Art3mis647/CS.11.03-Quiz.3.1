f1b9e9652950137645ce8a98ba8f4f532d58ee7d
tonyshu <tonynshu@gmail.com>
2023-10-26 09:28:21 +0800

    finished?

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,57 @@
 public class Main {
 
-    // Question 1 - calculateGrade
+    public static char calculateGrade(int grade){
+        if (grade >=90){
+            return 'A';
+        }
+        if (grade >=80){
+            return 'B';
+        }
+        if (grade >=70){
+            return 'C';
+        }
+        if (grade >=60){
+            return 'D';
+        }
+        if (grade >=50){
+            return 'E';
+        }
+        return 'F';
+    }
 
-    // Question 2 - fizzBuzz
+    public static String fizzBuzz(int n){
+        String returnString = "";
+        if (n%3 == 0){
+            returnString = returnString + "fizz";
+        }
+        if (n%5 == 0){
+            returnString = returnString + "buzz";
+        }
+        if (!(n%5 == 0) && !(n%3 == 0)){
+            returnString = returnString + "unlucky";
+        }
+        return returnString;
+    }
 
-    // Question 3 - frontBack
+    public static String frontBack(String n){
+        if(n.length()<2){
+            return n;
+        }
+        String nFront = n.substring (0, 2);
+        return nFront + n + nFront;
+    }
 
-    // Question 4 - twoAsOne
+    public static boolean twoAsOne(int a, int b, int c){
+        return (a + b == c) || (a + c == b) || (b + c == a);
+    }
 
-    // Question 5 - endUp
+    public static String endUp(String n){
+        if (n.length()<3){
+            return n.toUpperCase();
+        }
+        String nEnd = n.substring(n.length()-3).toUpperCase();
+        return n.substring(0, n.length()-3) + nEnd;
+
+    }
 
 }
